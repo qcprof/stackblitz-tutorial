3b194f7c896a19c4d00498079a1c3ccf882e7fba
qcprof <anique.qureshi@qc.cuny.edu>
2024-10-16 00:14:11 -0400

    second

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,7 +3,7 @@ export default function Hero(){
     <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content text-center">
     <div className="max-w-md">
-      <h1 className="text-5xl font-bold">Hello there</h1>
+      <h1 className="text-5xl font-bold">Hello Tutorial</h1>
       <p className="py-6">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
         quasi. In deleniti eaque aut repudiandae et a id nisi.
